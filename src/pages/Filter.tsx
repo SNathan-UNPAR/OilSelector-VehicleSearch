ab1ca874ddab2c160fb4eccdd2749a0102812d92
Samuel Surjo Saputro <sam080404@gmail.com>
2025-11-12 20:00:35 +0700

    Edit filter UI

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -10,48 +10,60 @@ function Filter(){
                     <img src="src/assets/filter.svg" alt="" />
                 </div>
                 <div className='country-dropdown'>
-                    <div className='country-dropdown-container unselected-option'>
+                    <div className='country-dropdown-container'>
                         <span>Country</span>
                     </div>
-                    <div className='bar unselected-bar'>
-                        <img src="src/assets/Dropdown1.svg" alt="" />
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
                     </div>
                 </div>
                 <div className='vehicleType-dropdown'>
-                    <div className='vehicleType-dropdown-container unselected-option'>
+                    <div className='vehicleType-dropdown-container'>
                         <span>Vehicle Type</span>
                     </div>
-                    <div className='bar unselected-bar'></div>
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
+                    </div>
                 </div>
                 <div className='manufacture-dropdown'>
-                    <div className='manufacture-dropdown-container unselected-option'>
+                    <div className='manufacture-dropdown-container'>
                         <span>Manufacture</span>
                     </div>
-                    <div className='bar unselected-bar'></div>
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
+                    </div>
                 </div>
                 <div className='model-dropdown'>
-                    <div className='model-dropdown-container unselected-option'>
+                    <div className='model-dropdown-container'>
                         <span>Model</span>
                     </div>
-                    <div className='bar unselected-bar'></div>
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
+                    </div>
                 </div>
                 <div className='engineSize-dropdown'>
-                    <div className='engineSize-dropdown-container unselected-option'>
+                    <div className='engineSize-dropdown-container'>
                         <span>Engine Size</span>
                     </div>
-                    <div className='bar unselected-bar'></div>
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
+                    </div>
                 </div>
                 <div className='year-dropdown'>
-                    <div className='year-dropdown-container unselected-option'>
+                    <div className='year-dropdown-container'>
                         <span>Year</span>
                     </div>
-                    <div className='bar unselected-bar'></div>
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
+                    </div>
                 </div>
                 <div className='fuelType-dropdown'>
-                    <div className='fuelType-dropdown-container unselected-option'>
+                    <div className='fuelType-dropdown-container'>
                         <span>Fuel Type</span>
                     </div>
-                    <div className='bar unselected-bar'></div>
+                    <div className='dropdown-img'>
+                        <img src="src/assets/dropdown.svg" alt="" />
+                    </div>
                 </div>
 
             </div>
